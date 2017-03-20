9e15b813e5609c6d2e43e5ff87e2999c6e8fe416
Erica Wright and Sarah Leahy <erica@>
2017-03-20 10:54:16 -0700

    create announcement model

---
+++ app/models/announcement.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  announcement: DS.attr(),
+  date: DS.attr()
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
+    return this.store.findAll('announcement');
   },
   actions:{
     saveRental(params){
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
+      this.transitionTo('index');
+    },
+    saveAnnouncement(params){
+      var newAnnouncement = this.store.createRecord('announcement', params);
+      newAnnouncement.save();
       this.transitionTo('index');
     }
   }
