e4b40aa9ea558d1851236370ddeb46147171046a
Erica Wright and Sarah Leahy <sarah@>
2017-03-20 11:57:45 -0700

    create announcement route with tile

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
+  model(rental) {
     return this.store.findAll('rental');
-    return this.store.findAll('announcement');
   },
   actions:{
     saveRental(params){
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
-      this.transitionTo('index');
-    },
-    saveAnnouncement(params){
-      var newAnnouncement = this.store.createRecord('announcement', params);
-      newAnnouncement.save();
       this.transitionTo('index');
     }
   }
